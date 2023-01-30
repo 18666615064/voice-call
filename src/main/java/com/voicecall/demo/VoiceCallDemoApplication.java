b4107b1b20ac5ceff3b37eebee3163874756202e
陈贤杰 <644596713@qq.com>
2023-01-30 16:32:14 +0800

    init

---
+++ src/main/java/com/voicecall/demo/VoiceCallDemoApplication.java
@@ -0,0 +1,13 @@
+package com.voicecall.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceCallDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoiceCallDemoApplication.class, args);
+    }
+
+}
